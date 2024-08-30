a645607c3ad8a2a6a2053c3b28ce0722bb13650e
Ilya <ilyaz3007@gmail.com>
2024-08-30 15:31:59 +0300

    delete useless import

--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -1,6 +1,5 @@
 import { Button, TextField, Typography, Checkbox, Box, FormControlLabel } from '@mui/material';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { IPropsLogin } from '../../common/types/auth';
 
 const LoginPage: React.FC<IPropsLogin> = (props: IPropsLogin): JSX.Element => {
